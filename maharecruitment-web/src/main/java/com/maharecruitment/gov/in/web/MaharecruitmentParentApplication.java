51984c6a2c06235859c78ee68fd9110f423e37a1
Shivram Mane <Shivram.Mane@Mahait.org>
2026-04-24 13:47:30 +0530

    changes for stag

--- maharecruitment-web/src/main/java/com/maharecruitment/gov/in/web/MaharecruitmentParentApplication.java
+++ maharecruitment-web/src/main/java/com/maharecruitment/gov/in/web/MaharecruitmentParentApplication.java
@@ -2,9 +2,9 @@ package com.maharecruitment.gov.in.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {
--- maharecruitment-web/src/main/java/com/maharecruitment/gov/in/web/error/CustomErrorController.java
+++ maharecruitment-web/src/main/java/com/maharecruitment/gov/in/web/error/CustomErrorController.java
@@ -1,7 +1,7 @@
 package com.maharecruitment.gov.in.web.error;
 
 
-import org.springframework.boot.webmvc.error.ErrorController;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
