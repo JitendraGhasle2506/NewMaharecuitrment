bc5412937dbc754e7e0541f872c13246bb572a55
Akanksha Surve <akanksha.egov@gmail.com>
2026-05-25 16:40:49 +0530

    changes

--- maharecruitment-recruitment/src/main/java/com/maharecruitment/gov/in/recruitment/dto/employee/TaskSubmissionForm.java
+++ maharecruitment-recruitment/src/main/java/com/maharecruitment/gov/in/recruitment/dto/employee/TaskSubmissionForm.java
@@ -11,7 +11,5 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class TaskSubmissionForm {
-    private String entryMode = "DAILY";
-    private LocalDate globalTaskDate;
     private List<EmployeeTaskLogDto> taskList = new ArrayList<>();
 }
