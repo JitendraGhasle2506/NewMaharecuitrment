fda5b8bafa81f4aac6f3fadb3b166db4099ed825
JitendraGhasle2506 <jitendraghasle.egov@gmail.com>
2026-07-17 12:31:03 +0530

    feat(security): Add TomcatHttpMethodConfig and corresponding test for connector availability

--- maharecruitment-web/src/main/java/com/maharecruitment/gov/in/web/config/TomcatHttpMethodConfig.java
+++ maharecruitment-web/src/main/java/com/maharecruitment/gov/in/web/config/TomcatHttpMethodConfig.java
@@ -1,5 +1,6 @@
 package com.maharecruitment.gov.in.web.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -7,7 +8,15 @@ import org.springframework.context.annotation.Configuration;
 
 /** Connector-level defence in depth for executable Spring Boot deployments. */
 @Configuration
+@ConditionalOnClass(name = {
+        TomcatHttpMethodConfig.TOMCAT_FACTORY_CLASS_NAME,
+        TomcatHttpMethodConfig.CATALINA_CONNECTOR_CLASS_NAME
+})
 public class TomcatHttpMethodConfig {
+
+    static final String TOMCAT_FACTORY_CLASS_NAME =
+            "org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory";
+    static final String CATALINA_CONNECTOR_CLASS_NAME = "org.apache.catalina.connector.Connector";
 
     @Bean
     WebServerFactoryCustomizer<TomcatServletWebServerFactory> disableTomcatTrace() {
